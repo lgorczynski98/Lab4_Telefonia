5111f547363b4107b49be61e4dbe0448bdc51f00
lgorczynski98 <lgorczynski98@gmail.com>
2019-01-13 00:33:46 +0100

    Pierwsza wersja testowa

---
+++ Lab4_Telefonia/Tkonto.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+class Tkonto
+{
+public:
+	
+	enum Eplec{kobieta, mezczyzna};
+	enum Edostep{uzytkownik, admin, brak};
+	
+private:
+
+	std::string login;
+	std::string haslo;
+	std::string imie;
+	std::string nazwisko;
+	std::string nr_tel;
+	Edostep dostep;
+	Eplec plec;
+
+public:
+
+	Tkonto();
+	Tkonto(std::string login, std::string haslo, std::string nr_tel, std::string imie, std::string nazwisko, Eplec plec, Edostep dostep);
+	std::vector<std::string> do_wyswietlenia();
+	int get_dostep();
+};
--- Lab4_Telefonia/Tsql.h
+++ Lab4_Telefonia/Tsql.h
@@ -6,6 +6,7 @@
 #include <mysql.h>
 #include <sstream>
 #include <vector>
+#include "Tkonto.h"
 
 class Tsql
 {
@@ -16,4 +17,8 @@ public:
 	Tsql();
 	~Tsql();
 	void wyswietl(std::function<std::string()>fun, std::vector<int>co_wypisac);
+	void modyfikuj(std::function<std::string()>fun, std::string komunikat);
+	std::string zwroc(std::function<std::string()>fun);
+	bool login_haslo(std::string login, std::string haslo);
+	Tkonto zwroc(std::string login, std::string haslo);
 };
